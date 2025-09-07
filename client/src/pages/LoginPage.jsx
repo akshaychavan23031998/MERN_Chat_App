f7ab55984d3eec2da0e5fd4f6e9ba00a83bd3406
akshaychavan23031998 <akshayrchavan07@gmail.com>
2025-09-07 19:46:43 +0530

    Formating.

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -21,7 +21,11 @@ const LoginPage = () => {
   return (
     <div className="min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl">
       {/* Left side */}
-      <img src={assets.logo_big} alt="Quickchat" className="shrink-0 w-[140px] sm:w-[200px] md:w-[240px] lg:w-[280px]" />
+      <img
+        src={assets.logo_big}
+        alt="Quickchat"
+        className="shrink-0 w-[140px] sm:w-[200px] md:w-[240px] lg:w-[280px]"
+      />
       {/* Right side */}
       <form
         onSubmit={onSubmitHandler}
