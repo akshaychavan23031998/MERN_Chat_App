b5b9c446a05d6aee6c2e511e7cf42e06440f26bb
akshaychavan23031998 <akshayrchavan07@gmail.com>
2025-09-11 23:50:05 +0530

    Integrated the Context API in frontend.

---
+++ client/context/AuthContext.jsx
@@ -0,0 +1,38 @@
+import { Children, createContext, useEffect, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+axios.defaults.baseURL = backendUrl;
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [authUser, setAuthUser] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const [socket, setSocket] = useState(null);
+
+  //   check if the user is authenticated and if so set the user data and connect the socket.
+  const checkAuth = async () => {
+    try {
+      const { data } = await axios.get("/api/auth/check");
+      if (data.success) {
+        setAuthUser(data.user);
+        toast.success("Your are In");
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      axios.defaults.headers.common["token"] = token;
+    }
+    checkAuth();
+  }, []);
+
+  const value = {};
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
